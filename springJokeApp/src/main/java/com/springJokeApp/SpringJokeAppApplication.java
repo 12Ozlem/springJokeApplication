0c36fd43b730d0b5bf63714daec85e2967d7e5bf
12Ozlem <oz.aydogmus@gmail.com>
2019-01-04 12:18:30 +0300

    Initial project commit

---
+++ springJokeApp/src/main/java/com/springJokeApp/SpringJokeAppApplication.java
@@ -0,0 +1,14 @@
+package com.springJokeApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJokeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJokeAppApplication.class, args);
+	}
+
+}
+
---
+++ springJokeApp/src/test/java/com/springJokeApp/SpringJokeAppApplicationTests.java
@@ -0,0 +1,17 @@
+package com.springJokeApp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringJokeAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
